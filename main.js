f453c389900b01e953c0b3f4fe4eb4d8d73f190e
saifsammy <saifsammy653@gmail.com>
2024-03-26 11:17:53 +0600

    learned esm and common js module

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 // how to import so many files from a folder
-const { loveAdd, loveSubstract } = require("./utils")
+// const { loveAdd, loveSubstract } = require("./utils")
 
 
-console.log(loveAdd(5, 10), loveSubstract(50, 10));
+// console.log(loveAdd(5, 10), loveSubstract(50, 10));
+
+
+
+
+// IIFE
+// Immediately Invoked Function Expression
+
+((node) => {
+    console.log(`I will level up my skills in ${node}`);
+})("level 2")
+
+
+
+// If I want to use import instant of require than I have to rename the file extension .mjs instant of .js than I will use import & export instant of module.exports & require
