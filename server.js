bec6f794096d3b922afda0cdc5b2ac986cd1dd53
saifsammy <saifsammy653@gmail.com>
2024-03-28 12:19:59 +0600

    basic routing with node.js

--- server.js
+++ server.js
@@ -3,7 +3,17 @@ const http = require("node:http")
 
 const server = http.createServer((req, res) => {
     // res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World!');
+    console.log(req.url, req.method);
+
+    if (req.url === "/home" && req.method === "GET") {
+        res.end("This is home route")
+    } else if (req.url === "/post" && req.method === "GET") {
+        res.end("This data from post")
+    } else {
+        res.end("Not Found")
+    }
+
+    // res.end('Hello World!');
 });
 
 // starts a simple http server locally on port 3000
