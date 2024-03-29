dc524b085367250982365b4f6ed822e6def2ab1d
saifsammy <saifsammy653@gmail.com>
2024-03-29 12:40:10 +0600

    create crud operation with node.js

--- server.js
+++ server.js
@@ -1,94 +1,126 @@
-// server.js
-const http = require("node:http")
-const fs = require("fs")
+const http = require("http");
+const fs = require("fs");
+const { MongoClient, ObjectId } = require("mongodb");
 
-// sample data
-// const post = [
-//     {
-//         id: 1,
-//         title: "Blog Post 1",
-//         content: "This is the content of the first blog post.",
-//         author: "John Doe",
-//         date: "2024-03-28",
-//     },
-//     {
-//         id: 2,
-//         title: "Another Blog Post",
-//         content: "Here's some content for the second blog post.",
-//         author: "Jane Smith",
-//         date: "2024-03-27",
-//     },
-//     {
-//         id: 3,
-//         title: "A Cool Article",
-//         content: "Check out this interesting article content!",
-//         author: "Alice Jones",
-//         date: "2024-03-26",
-//     },
-//     {
-//         id: 4,
-//         title: "Amazing Story",
-//         content: "Immerse yourself in this captivating story.",
-//         author: "Bob Brown",
-//         date: "2024-03-25",
-//     },
-// ];
+const port = process.env.PORT || 5000;
 
+const uri = 'mongodb://0.0.0.0:27017/';
+const client = new MongoClient(uri);
 
+async function run() {
+    try {
+        await client.connect();
+        const postCollection = client.db("post").collection("allPost");
 
+        const server = http.createServer(async (req, res) => {
+            console.log(req.url, req.method);
 
-const server = http.createServer((req, res) => {
-    // res.writeHead(200, { 'Content-Type': 'text/plain' });
-    console.log(req.url, req.method);
+            const parsedURL = new URL(req.url, `http://${req.headers.host}`);
+            const pathName = parsedURL.pathname;
 
-    const parsedURL = new URL(req.url, `http://${req.headers.host}`);
+            if (pathName === "/home" && req.method === "GET") {
+                fs.readFile(__dirname + "/index.html", "utf-8", (err, data) => {
+                    if (err) {
+                        res.writeHead(500, { 'Content-Type': 'text/plain' });
+                        res.end("Error! Reading File");
+                    } else {
+                        res.setHeader("Content-type", "text/html");
+                        res.statusCode = 200;
+                        res.end(data);
+                    }
+                });
+            } else if (pathName === "/post" && req.method === "GET") {
+                fs.readFile(__dirname + "/data.json", "utf-8", (err, data) => {
+                    if (err) {
+                        res.writeHead(500, { 'Content-Type': 'text/plain' });
+                        res.end("Error! Reading File");
+                    } else {
+                        const query = parsedURL.searchParams;
+                        const postID = query.get("id");
+                        const expectedPost = JSON.parse(data).find(p => p.id == postID);
+                        res.setHeader("Content-type", "application/json");
+                        res.statusCode = 200;
+                        res.end(JSON.stringify(expectedPost));
+                    }
+                });
 
-    const pathName = parsedURL.pathname
+                // post method
+            } else if (pathName === "/create-post" && req.method === "POST") {
+                let body = "";
+                req.on("data", (chunk) => {
+                    body += chunk.toString();
+                });
 
+                req.on("end", async () => {
+                    try {
+                        const postData = JSON.parse(body);
+                        const result = await postCollection.insertOne(postData);
+                        res.setHeader("Content-type", "application/json");
+                        res.statusCode = 200;
+                        res.end(JSON.stringify({
+                            message: "Post created successfully!",
+                            data: result.ops[0], // Return the inserted document
+                        }));
+                    } catch (error) {
+                        console.error("Error parsing JSON:", error);
+                        res.writeHead(400, { 'Content-Type': 'text/plain' });
+                        res.end("Error! Invalid JSON");
+                    }
+                });
 
+                // updated post
+            } else if (pathName.startsWith("/update-post") && req.method === "PATCH") {
+                const postID = pathName.split("/")[2];
+                let body = "";
+                req.on("data", (chunk) => {
+                    body += chunk.toString();
+                });
 
-    if (pathName === "/home" && req.method === "GET") {
-        fs.readFile(__dirname + "/index.html", "utf-8", (err, data) => {
-            if (err) {
-                throw new Error("Error! Reading File");
+                req.on("end", async () => {
+                    try {
+                        const updatedData = JSON.parse(body);
+                        const result = await postCollection.updateOne(
+                            { _id: new ObjectId(postID) },
+                            { $set: updatedData });
+                        res.setHeader("Content-type", "application/json");
+                        res.statusCode = 200;
+                        res.end(JSON.stringify({
+                            message: "Post updated successfully!",
+                            data: result.ops[0], // Return the inserted document
+                        }));
+                    } catch (error) {
+                        console.error("Error parsing JSON:", error);
+                        res.writeHead(400, { 'Content-Type': 'text/plain' });
+                        res.end("Error! Invalid JSON");
+                    }
+                });
+            } else if (pathName.startsWith("/delete-post") && req.method === "DELETE") {
+                const postID = pathName.split("/")[2];
+
+                const result = await postCollection.deleteOne(
+                    { _id: new ObjectId(postID) },
+                );
+                res.setHeader("Content-type", "application/json");
+                res.statusCode = 200;
+                res.end(JSON.stringify({
+                    message: "Post deleted successfully!",
+                    data: result.ops[0], // Return the inserted document
+                }));
+
+            } else {
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.end("Not Found");
             }
-            res.setHeader("Content-type", "text/html");
-            res.statusCode = 200;
-            // const data = JSON.stringify(expectedPost)
-            res.end(data)
-        })
-    } else if (pathName === "/post" && req.method === "GET") {
-        // res.writeHead(200, {
-        //     "Content-type": "application/json",
-        //     email: "robin@gmail.com",
-        // })
+        });
 
+        server.listen(port, () => {
+            console.log(`Server is Listening on ${port}`);
+        });
 
-        fs.readFile(__dirname + "/data.json", "utf-8", (err, data) => {
-            if (err) {
-                throw new Error("Error! Reading File");
-            }
-            const query = parsedURL.searchParams
-            const postID = query.get("id")
-
-            const expectedPost = JSON.parse(data).find(p => p.id == postID)
-            res.setHeader("Content-type", "application/json");
-            res.statusCode = 200;
-            res.end(JSON.stringify(expectedPost))
-        })
-
-
-
-    } else {
-        res.end("Not Found")
+    } finally {
+        // Close the MongoDB client when done
+        // client.close();
     }
+}
 
-    // res.end('Hello World!');
-});
-
-// starts a simple http server locally on port 3000
-server.listen(5000, '127.0.0.1', () => {
-    console.log('Server is Listening on 5000');
-});
-
-// run with `node server.js`
+run().catch(console.error);
