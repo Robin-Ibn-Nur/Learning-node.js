b3d51ffc9075cc659743e075dad0df4b5a91d1c8
saifsammy <saifsammy653@gmail.com>
2024-03-29 11:24:22 +0600

    learned query perameter using file system module

--- server.js
+++ server.js
@@ -1,37 +1,38 @@
 // server.js
 const http = require("node:http")
+const fs = require("fs")
 
 // sample data
-const post = [
-    {
-        id: 1,
-        title: "Blog Post 1",
-        content: "This is the content of the first blog post.",
-        author: "John Doe",
-        date: "2024-03-28",
-    },
-    {
-        id: 2,
-        title: "Another Blog Post",
-        content: "Here's some content for the second blog post.",
-        author: "Jane Smith",
-        date: "2024-03-27",
-    },
-    {
-        id: 3,
-        title: "A Cool Article",
-        content: "Check out this interesting article content!",
-        author: "Alice Jones",
-        date: "2024-03-26",
-    },
-    {
-        id: 4,
-        title: "Amazing Story",
-        content: "Immerse yourself in this captivating story.",
-        author: "Bob Brown",
-        date: "2024-03-25",
-    },
-];
+// const post = [
+//     {
+//         id: 1,
+//         title: "Blog Post 1",
+//         content: "This is the content of the first blog post.",
+//         author: "John Doe",
+//         date: "2024-03-28",
+//     },
+//     {
+//         id: 2,
+//         title: "Another Blog Post",
+//         content: "Here's some content for the second blog post.",
+//         author: "Jane Smith",
+//         date: "2024-03-27",
+//     },
+//     {
+//         id: 3,
+//         title: "A Cool Article",
+//         content: "Check out this interesting article content!",
+//         author: "Alice Jones",
+//         date: "2024-03-26",
+//     },
+//     {
+//         id: 4,
+//         title: "Amazing Story",
+//         content: "Immerse yourself in this captivating story.",
+//         author: "Bob Brown",
+//         date: "2024-03-25",
+//     },
+// ];
 
 
 
@@ -44,37 +45,40 @@ const server = http.createServer((req, res) => {
 
     const pathName = parsedURL.pathname
 
+
+
     if (pathName === "/home" && req.method === "GET") {
-        res.writeHead(200, {
-            "Content-type": "text/html",
+        fs.readFile(__dirname + "/index.html", "utf-8", (err, data) => {
+            if (err) {
+                throw new Error("Error! Reading File");
+            }
+            res.setHeader("Content-type", "text/html");
+            res.statusCode = 200;
+            // const data = JSON.stringify(expectedPost)
+            res.end(data)
         })
-        res.end(`<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <h1>This is LOVE HTML</h1>
-</body>
-
-</html>`)
     } else if (pathName === "/post" && req.method === "GET") {
         // res.writeHead(200, {
         //     "Content-type": "application/json",
         //     email: "robin@gmail.com",
         // })
-        const query = parsedURL.searchParams
-        const postID = query.get("id")
 
-        const expectedPost = post.find(p => p.id == postID)
-        res.setHeader("Content-type", "application/json");
-        res.statusCode = 200;
-        const data = JSON.stringify(expectedPost)
-        res.end(data)
+
+        fs.readFile(__dirname + "/data.json", "utf-8", (err, data) => {
+            if (err) {
+                throw new Error("Error! Reading File");
+            }
+            const query = parsedURL.searchParams
+            const postID = query.get("id")
+
+            const expectedPost = JSON.parse(data).find(p => p.id == postID)
+            res.setHeader("Content-type", "application/json");
+            res.statusCode = 200;
+            res.end(JSON.stringify(expectedPost))
+        })
+
+
+
     } else {
         res.end("Not Found")
     }
